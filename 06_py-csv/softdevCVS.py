b19441aac8562708e4392837bb37973be5b060d8
zou-Justin <58957203+zou-Justin@users.noreply.github.com>
2021-09-28 18:56:57 -0400

    updating with new code

--- 06_py-csv/softdevCVS.py
+++ 06_py-csv/softdevCVS.py
@@ -1,14 +1,23 @@
+# Mark Zhu, Justin Zou, Han Zhang (Team MJH)
+# SoftDev
+# K06 -- StI/O: Divine your Destiny!
+# 2021-09-28
+
 import random
 import csv
 
-def main():
-    randomNum = random.random()* 99.8
+
+randomNum = random.random()* 99.8
+dict = {}
     
-    with open("occupations.csv", newline='') as csvfile:
-        Dictionary = csv.DictReader(csvfile)
-    x = 0.0
-    for i, j in Dictionary.items():
-        if x <= randomNum < x + j:
-            print (i)
-        x += j
-        
+with open("occupations.csv", mode = 'r') as csvfile:
+    csv = csv.DictReader(csvfile)
+
+    for lines in csv:
+        dict[lines['Job Class']] = float(lines['Percentage'])
+
+x = 0.0
+for i, j in dict.items():
+    if x <= randomNum < x + j:
+        print (i)
+    x += j
