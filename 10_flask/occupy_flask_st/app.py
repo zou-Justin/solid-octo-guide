1193445d53ec235994e3e9af325c6c8b44db7134
Justin <justin82049@gmail.com>
2021-10-04 21:10:10 -0400

    understanding that it only runs the top function

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -1,10 +1,29 @@
 from flask import Flask
+
+import random
+import csv
+
 app = Flask(__name__)
 
 @app.route("/") 
 
+def main(): 
+    return heading() + "\n" + listoccupations()
+
 def heading():
     return "<h3> Mark Zhu, Justin Zou, Han Zhang </h3>"
+
+def listoccupations(): 
+    dict = {}
+    with open("occupations.csv", mode = 'r') as csvfile:
+        file = csv.DictReader(csvfile)
+
+        for lines in file:
+            dict[lines['Job Class']] = float(lines['Percentage'])
+    
+    dict.pop('Total')
+
+    return "<br/><br/><b>List of occupations: </b> <br/>" + "<br />".join(dict.keys())
 
 app.run() 
                 
