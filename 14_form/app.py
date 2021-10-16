be30ce466bcec3308e35e2d1b8c716ae8fa46b00
zou-Justin <58957203+zou-Justin@users.noreply.github.com>
2021-10-16 19:15:35 -0400

    Update app.py

--- 14_form/app.py
+++ 14_form/app.py
@@ -27,15 +27,15 @@ PROTIP: Insert your own in-line comments wherever they will help your future sel
 def disp_loginpage(): # function to display the initial login page
     print("\n\n\n")
     print("***DIAG: this Flask obj ***") 
-    print(app) # prints "<Flask 'app'>"
+    print(app)
     print("***DIAG: request obj ***") 
     print(request) # prints "<Request 'http://127.0.0.1:5000/' [GET]>" which is the information about the form request (request method used)
     print("***DIAG: request.args ***") 
-    print(request.args) # prints a dictionary with the inputs of the user, which is empty right now - "ImmutableMultiDict([])"
+    print(request.args) # prints a dictionary with the inputs of the user, which is empty right now
     # print("***DIAG: request.args['username']  ***") 
     # print(request.args['username']) # this creates a KeyError because request.args is empty
     print("***DIAG: request.headers ***")  
-    print(request.headers) # prints out a lot of information such as the host, platform being used, and connection
+    print(request.headers) 
     return render_template( 'login.html' ) # serves template from localhost
 
 
@@ -43,17 +43,17 @@ def disp_loginpage(): # function to display the initial login page
 def authenticate(): # function that runs after the user submits their inputs 
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
-    print(app) # same as previous function
+    print(app) 
     print("***DIAG: request obj ***") 
     print(request) # almost the same as previous function, but it also has "auth?" followed by the values of 'username' and 'sub1' (which is just 'Submit')
     print("***DIAG: request.args ***")
     print(request.args) # same as previous function except this time here are entries for the values of 'username' and 'sub1' (which is just 'Submit')
     # print("***DIAG: request.args['username']  ***")
-    # print(request.args['username']) # this prints out the value of 'username' which was provided by the user
+    # print(request.args['username']) # this prints out the username provided by the user
     print("***DIAG: request.headers ***")
-    print(request.headers) # same as previous function except with some different values such as the referer which is just the URL of localhost
-    # return "Waaaa hooo HAAAH"  #response to a form submission
-    return render_template('response.html', username=request.args['username'], method=request.method)
+    print(request.headers) 
+    return render_template('response.html', username=request.args['username'], method=request.method) #what is shown on our response page, using response.html as our template
+   
 
 
     
