731c456e140d33a6b033caf7f5f154bd92624b3b
zou-Justin <58957203+zou-Justin@users.noreply.github.com>
2021-10-16 19:07:24 -0400

    Update app.py

--- 14_form/app.py
+++ 14_form/app.py
@@ -1,6 +1,7 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Oct 2021 
+# Selective Soup | Justin Zou (Piggy), Wen Hao Dong (Jal Hordan), Roshani Shrestha (Pete)
+# SoftDev 
+# K14: Form and Function - Displays login and authenticate pages 
+# 2021-10-15 
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
@@ -23,35 +24,36 @@ PROTIP: Insert your own in-line comments wherever they will help your future sel
 '''
 
 @app.route("/") #, methods=['GET', 'POST'])
-def disp_loginpage():
+def disp_loginpage(): # function to display the initial login page
     print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    return render_template( 'login.html' )
+    print("***DIAG: this Flask obj ***") 
+    print(app) # prints "<Flask 'app'>"
+    print("***DIAG: request obj ***") 
+    print(request) # prints "<Request 'http://127.0.0.1:5000/' [GET]>" which is the information about the form request (request method used)
+    print("***DIAG: request.args ***") 
+    print(request.args) # prints a dictionary with the inputs of the user, which is empty right now - "ImmutableMultiDict([])"
+    # print("***DIAG: request.args['username']  ***") 
+    # print(request.args['username']) # this creates a KeyError because request.args is empty
+    print("***DIAG: request.headers ***")  
+    print(request.headers) # prints out a lot of information such as the host, platform being used, and connection
+    return render_template( 'login.html' ) # serves template from localhost
 
 
 @app.route("/auth") # , methods=['GET', 'POST'])
-def authenticate():
+def authenticate(): # function that runs after the user submits their inputs 
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
+    print(app) # same as previous function
+    print("***DIAG: request obj ***") 
+    print(request) # almost the same as previous function, but it also has "auth?" followed by the values of 'username' and 'sub1' (which is just 'Submit')
     print("***DIAG: request.args ***")
-    print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    print(request.args) # same as previous function except this time here are entries for the values of 'username' and 'sub1' (which is just 'Submit')
+    # print("***DIAG: request.args['username']  ***")
+    # print(request.args['username']) # this prints out the value of 'username' which was provided by the user
     print("***DIAG: request.headers ***")
-    print(request.headers)
-     return render_template('response.html', username=request.args['username'], method=request.method)
+    print(request.headers) # same as previous function except with some different values such as the referer which is just the URL of localhost
+    # return "Waaaa hooo HAAAH"  #response to a form submission
+    return render_template('response.html', username=request.args['username'], method=request.method)
 
 
     
