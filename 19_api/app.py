462f7ebfe8ad2e525e63f27127c1cdec4ba3ce4b
Justin <justin82049@gmail.com>
2021-11-23 20:28:15 -0500

    work so far

--- 19_api/app.py
+++ 19_api/app.py
@@ -4,12 +4,20 @@
 # 2021-11-23
 
 from flask import Flask, render_template
+import urllib3
+import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return render_template("main.html",pic = "https://apod.nasa.gov/ap…ctiveSun_NuSTAR_1600.jpg")
+    url = "https://api.nasa.gov/planetary/apod?api_key=7FDdoAzbN5DoWCsTmAqZz3NIeHSGgaDd6nxUTvWJ"
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'url')
+    w = json.loads(r.data)
+    pic = w.get("url")
+    return render_template("main.html",pic = pic)
 
 
 if __name__ == "__main__":
