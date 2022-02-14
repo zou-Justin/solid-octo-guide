5a62214bd4b22921df228d785f2e7656cec18f7e
zou-justin <justin82049@gmail.com>
2022-02-14 09:23:43 -0500

    classwork

---
+++ 30_jsdraw/clickdraw.js
@@ -0,0 +1,23 @@
+// Team Tofu :: Justin Zou, Annabel Zhang
+// SoftDev pd2
+// K30: JSDraw
+// 2022-02-14t
+// --------------------------------------------------
+
+
+ var c = document.getElementById("slate");
+
+ var ctx = c.getContent("2d");
+
+ var mode = "rect";
+
+ var toggleMode = (e) => {
+   console.log(toggling);
+   var btT = document.getElementById("buttonToggle");
+   if (btT){
+    
+   }
+   else{
+
+   }
+ }
