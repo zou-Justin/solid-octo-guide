f91f0f7e07bb2c9f4055ba3404fedf8e62410371
zou-justin <justin82049@gmail.com>
2022-02-15 09:28:05 -0500

    work

---
+++ 31_js-anim/animate.js
@@ -0,0 +1,84 @@
+// Team Tofu :: Justin Zou, Annabel Zhang
+// SoftDev pd2
+// K31: JSAnim
+// 2022-02-15t
+// --------------------------------------------------
+
+
+// model for HTML5 canvas-based animation
+
+// SKEELTON
+
+
+//access canvas and buttons via DOM
+var c = document.getElementById("playground");
+var dotButton = document.getElementById("buttonCircle");
+var stopButton = document.getElementById("buttonStop");
+
+//prepare to interact with canvas in 2D
+var ctx = c.getContext("2d");
+
+//set fill color to team color
+ctx.fillStyle = "#f6e8e0"
+
+var requestID;  //init global var for use with animation frames
+
+
+//var clear = function(e) {
+var clear = (e) => {
+  console.log("clear invoked...")
+  ctx.clearRect(0, 0, c.clientWidth, c.clientHeight);
+  // YOUR CODE HERE
+};
+
+
+var radius = 0;
+var growing = true;
+
+
+//var drawDot = function() {
+var drawDot = () => {
+  console.log("drawDot invoked...")
+  console.log(radius);
+  ctx.beginPath();
+  ctx.arc(c.clientWidth/2, c.clientHeight/2, radius, 0, 360);
+  ctx.fill();
+  radius+=1;
+  requestID = window.requestAnimationFrame(drawDot);
+
+  // YOUR CODE HERE
+
+  /*
+    ...to
+    Wipe the canvas,
+    Repaint the circle,
+
+    ...and somewhere (where/when is the right time?)
+    Update requestID to propagate the animation.
+    You will need
+    window.cancelAnimationFrame()
+    window.requestAnimationFrame()
+
+   */
+};
+
+
+//var stopIt = function() {
+var stopIt = () => {
+  console.log("stopIt invoked...")
+  console.log( requestID );
+  window.cancelAnimationFrame(requestID);
+
+  // YOUR CODE HERE
+  /*
+    ...to
+    Stop the animation
+
+    You will need
+    window.cancelAnimationFrame()
+  */
+};
+
+
+dotButton.addEventListener( "click", drawDot );
+stopButton.addEventListener( "click",  stopIt );
